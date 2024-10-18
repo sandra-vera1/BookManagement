7912d4f00c36d11510aa2fc0f6ef8c3131fb14f5
paulacode26 <pgperez26@gmail.com>
2024-10-18 12:44:16 -0600

    Added Backend folder

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { fileAuth } = require('./auth');
+const booksRouter = require('./routes/books');
+
+const app = express();
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+
+    const { username, password } = req.body;
+    // get user from file
+    const user = fileAuth(username, password);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    res.json({ message: 'Login successful' });
+
+});
+
+app.use('/books', booksRouter);
+
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
